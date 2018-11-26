7a9bae95dbbdb78386fb4d674fafb5d4f76e9dff
Peter Starup <pete59f1@edu.eal.dk>
2018-11-26 09:17:15 +0100

    Add project files.

---
+++ Ex28 CSharp med database/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ex28_CSharp_med_database
+{
+    public class Menu
+    {
+        private static string conntectionString =
+        "Server = ealSQL1.eal.local; Database = A_DB26_2018; User Id = A_STUDENT26; Password = A_OPENDB26;";
+        private void Show()
+        {
+            Console.WriteLine("YELLO");
+            Console.WriteLine("1. Insert a Pet");
+            Console.WriteLine("0. Exit");
+        }
+
+        public void RunMenu()
+        {
+            Show();
+            bool run = true;
+            string input = Console.ReadLine();
+            using (SqlConnection con = new SqlConnection(conntectionString))
+            {
+
+                do
+                {
+                    switch (input)
+                    {
+                        case "1":
+                            Console.WriteLine("Hey");
+                            break;
+                        case "0":
+                            run = false;
+                            break;
+                        default:
+                            Console.WriteLine("??");
+                            break;
+                    }
+
+                } while (run);
+            }
+        }
+    }
+}
---
+++ Ex28 CSharp med database/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex28_CSharp_med_database
+{
+    class Program
+    {
+        Menu menu = new Menu();
+
+        static void Main(string[] args)
+        {
+            Program pro = new Ex28_CSharp_med_database.Program();
+            pro.Run();
+        }
+
+        private void Run()
+        {
+            menu.RunMenu();
+        }
+    }
+}
